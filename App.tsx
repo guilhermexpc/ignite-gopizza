82c9c799273a95663fc7db5197eb454aca1969b6
Guilherme Campos <guilhermee1219@gmail.com>
2022-04-12 23:30:12 -0300

    tela SignIn e status bar

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { View } from 'react-native';
 import AppLoading from 'expo-app-loading';
+import { StatusBar } from 'expo-status-bar'
 import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
 import { ThemeProvider } from 'styled-components/native';
 
-import theme from '@src/theme';
+import { SignIn } from '@screens/SignIn';
+
+import theme from './src/theme';
 
 export default function App() {
 
@@ -18,10 +19,10 @@ export default function App() {
   if(!fontsLoaded)
     return <AppLoading />
 
-
   return (
     <ThemeProvider theme={theme}>
-
+      <StatusBar style='light' translucent={true} backgroundColor="transparent" />
+      <SignIn />
     </ThemeProvider>
   );
 }
