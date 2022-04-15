1162c29f4bea1258362c03b7660b54786dde1078
Guilherme Campos <guilhermee1219@gmail.com>
2022-04-15 13:41:31 -0300

    melhorias no layout do signin

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -4,28 +4,30 @@ import { Text } from 'react-native';
 import { Input } from '@components/input';
 import { Button } from '@components/Button';
 
-import { Container, ContainerAlter } from './styles';
+import { Container, ContainerAlter, Content } from './styles';
 
 export function SignIn(){
   return (
     <ContainerAlter>
-      <Input 
-        placeholder='E-mail'
-        type='secondary'
-        autoCorrect={false}
-        autoCapitalize="none"
-      />
+      <Content>
+        <Input 
+          placeholder='E-mail'
+          type='secondary'
+          autoCorrect={false}
+          autoCapitalize="none"
+        />
 
-      <Input 
-        placeholder='Senha'
-        type='secondary'
-        secureTextEntry={true}
-      />
+        <Input 
+          placeholder='Senha'
+          type='secondary'
+          secureTextEntry={true}
+        />
 
-      <Button
-        tittle="Entrar"
-        type="secondary"
-      />
+        <Button
+          tittle="Entrar"
+          type="secondary"
+        />
+      </Content>
     </ContainerAlter>
   );
 }
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,6 +1,18 @@
 import styled from "styled-components/native";
 import { LinearGradient, LinearGradientProps } from "expo-linear-gradient";
 import theme from '../../theme'
+import { ScrollViewProps } from "react-native";
+
+
+export const Content = styled.ScrollView.attrs(
+  (): ScrollViewProps => ({
+    showsVerticalScrollIndicator: false
+
+  }))`
+    width: 100%;
+    padding: 0 32px;
+  `
+
 
 export const Container = styled(LinearGradient).attrs<LinearGradientProps>(({ theme }) => ({
   colors: theme.COLORS.GRADIENT,
